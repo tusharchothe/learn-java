81c24407eef8165d12eb1e436bbe61a941fbcd20
tusharchothe <tusharchothe9@gmail.com>
2026-02-08 10:15:16 +0530

    updated code

---
+++ stringbuilder.java
@@ -0,0 +1,7 @@
+public class stringbuilder {
+    public static void main(String[] args) {
+        
+    }
+
+    
+}
--- strings.java
+++ strings.java
@@ -5,7 +5,9 @@ public class strings {
         //basicstring();
         //inputstring();
         //concat();
-        charat();
+        //charat();
+        //comparestring();
+        substring();
     }
 
     public static void basicstring(){
@@ -37,5 +39,44 @@ public class strings {
             System.out.println(name.charAt(i));
         }
     }
+
+    public static void comparestring(){
+        //comapre 
+        String str1 ="tushar";
+        String str2 ="tushar";
+
+        //1. s1>s2  return +ve 
+        //2. s1==s2  return 0 
+        //1. s1<s2  return -ve 
+        if(str1.compareTo(str2)==0){
+            System.out.println("strings are equal");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+
+        //by using equal to method 
+        if(str1 == str2){
+            System.out.println("strings are eaual");
+        }else{
+            System.out.println("Strings are not equal");
+        }
+
+        // above method is not reiable in most of the cases
+
+        if(new String("tushar")== new String("tushar")){
+            System.out.println("strings are equal");
+        }else{
+            System.out.println("strings are not equal");
+        }
+    }
+
+    public static void substring(){
+        String sen ="my name is tushar";
+        String name =sen.substring(11, sen.length());
+
+        System.out.println(name);
+    }
+     
+    // Strings are immutable  
     
 }
