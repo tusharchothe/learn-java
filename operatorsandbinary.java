11e4c5f2264bba974f7e5e892ff1bbdac6960cdc
tusharchothe <tusharchothe9@gmail.com>
2026-02-13 00:00:05 +0530

    operators

--- operatorsandbinary.java
+++ operatorsandbinary.java
@@ -1,7 +1,18 @@
+import java.util.*;
+
 public class operatorsandbinary {
     public static void main(String[] args) {
-        
+        getbit();
     }
     
-    
+    public static void getbit(){
+        int n =5;
+        int pos = 2;
+        int bitmask = 1<<pos;
+        if((bitmask & n)==0){
+            System.out.println("bit was zero");
+        }else{
+            System.out.println("bit is one");
+        }
+    }
 }
