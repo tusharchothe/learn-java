1a8febafce959bc4db021950c978f99ee4e51822
tusharchothe <tusharchothe9@gmail.com>
2026-02-14 15:32:37 +0530

    kuch naya nhi hai

--- operatorsandbinary.java
+++ operatorsandbinary.java
@@ -2,7 +2,10 @@ import java.util.*;
 
 public class operatorsandbinary {
     public static void main(String[] args) {
-        getbit();
+        // getbit();
+        // setbit();
+        // clearbit();
+        updatebitop();
     }
     
     public static void getbit(){
@@ -14,5 +17,50 @@ public class operatorsandbinary {
         }else{
             System.out.println("bit is one");
         }
+    }
+
+    public static void setbit(){
+        int n =5;
+        int pos =1;
+        int bitmask =1<<pos;
+
+        int newNumber = bitmask | n;
+      
+        System.out.println(newNumber);
+        
+    }
+
+    public static void clearbit(){
+        int n  =5;
+        int pos =2;
+        int bitmask =1<<pos;
+
+        int notbitmask = ~(bitmask);
+        int newNumber = notbitmask & n;
+        System.out.println(newNumber);
+    }
+
+    public static void updatebitop(){
+        Scanner sc  =new Scanner(System.in);
+        System.out.print("enter the operation :");
+        int op =sc.nextInt();
+
+        int n=5;
+        int pos =1;
+
+        int bitmask =1<<pos;
+        
+
+        if(op == 1){
+            
+            int newNumber = bitmask | n;
+            System.out.print(newNumber);
+        }else if(op == 0){
+            int notbitmask =~(bitmask);
+            int newNumber = notbitmask & n;
+            System.out.print(newNumber);
+        }
+
+
     }
 }
---
+++ sorting.java
@@ -0,0 +1,18 @@
+public class sorting {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void BubbleSort(){
+        // bubble sort 
+        
+
+    }
+
+    public static void SelectionSort(){
+    }
+
+    public static void InsertionSort(){
+
+    }
+}
